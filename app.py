827cdd6a8b4a4bb8d05c377e88dcdf03d71e9927
manojbadhan <manjitbadhan@gmail.com>
2026-03-11 17:54:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ import streamlit as st
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
+import gdown
+import os
 
-# Load trained model
-model = load_model("lung_cancer_model.h5")
+# -------- Download model from Google Drive if not present --------
+
+model_path = "lung_cancer_model.h5"
+
+if not os.path.exists(model_path):
+file_id = "1jFzKMIzz82r_AZ-T-eJOVxI2b4nQIVbJ"
+url = f"https://drive.google.com/uc?id={file_id}"
+gdown.download(url, model_path, quiet=False)
+
+# -------- Load trained model --------
+
+model = load_model(model_path)
 
 classes = ["Normal", "Benign", "Malignant"]
 
@@ -14,25 +26,27 @@ st.write("Upload a lung CT scan image to detect cancer.")
 uploaded_file = st.file_uploader("Upload CT Scan", type=["jpg","png","jpeg"])
 
 if uploaded_file is not None:
-    
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    img = cv2.imdecode(file_bytes, 1)
 
-    st.image(img, caption="Uploaded CT Scan", use_column_width=True)
+```
+file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+img = cv2.imdecode(file_bytes, 1)
 
-    img = cv2.resize(img,(128,128))
-    img = img/255.0
-    img = np.reshape(img,(1,128,128,3))
+st.image(img, caption="Uploaded CT Scan", use_container_width=True)
 
-    prediction = model.predict(img)
+img = cv2.resize(img,(128,128))
+img = img/255.0
+img = np.reshape(img,(1,128,128,3))
 
-    result = classes[np.argmax(prediction)]
+prediction = model.predict(img)
 
-    st.subheader("Prediction Result")
+result = classes[np.argmax(prediction)]
 
-    if result == "Malignant":
-        st.error("Malignant (Cancer Detected)")
-    elif result == "Benign":
-        st.warning("Benign Tumor Detected")
-    else:
-        st.success("Normal Lung")
+st.subheader("Prediction Result")
+
+if result == "Malignant":
+    st.error("Malignant (Cancer Detected)")
+elif result == "Benign":
+    st.warning("Benign Tumor Detected")
+else:
+    st.success("Normal Lung")
+```
